cd1a9b467991ed094acdb3cdd078acf86a235151
Yomely <yomelydigital@gmail.com>
2024-03-06 16:10:00 -0500

    resuelto errores de instalacion

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,17 +1,30 @@
+
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    const palabras = text.value.split("");
-   return palabras;
-
+    const arregpalabras = text.split(" ");
+    let contadorpalabras = 0; //Es una variable local para ver sólo en está función y la vamos a utilizar para indicar 
+    for (let i = 0; i < arregpalabras.length; i++) {
+      if (arregpalabras[i] !== "") {
+        contadorpalabras = contadorpalabras + 1;
+      }
+    }
+    return contadorpalabras;
   }, 
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-return text.length;
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
- 
+    const arregpalabras = text.split("");
+    let contadorpalabras = 0;
+    for (let i = 0; i < arregpalabras.length; i++) {
+      if (arregpalabras[i] !== " ") {
+        contadorpalabras = contadorpalabras + 1;
+      }
+    }
+    return contadorpalabras;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -2,30 +2,30 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 const cajaTexto = document.querySelector("textarea");
-const palabra = document.getElementById("palabras");
-const caracteres = document.getElementById("caracteres");
-const caracteresSinEspacios = document.getElementById("caracteresSinEspacios");
-const numeros = document.getElementById("numeros");
-const suma = document.getElementById("suma");
-const longitudMedia = document.getElementById("longitudMedia");
-const button = document.getElementById("reset-button");
+const palabra = document.querySelector("li[data-testid=word-count]");
+const caracteres = document.querySelector("li[data-testid=character-count]");
+const caracteresSinEspacios = document.querySelector("li[data-testid=character-no-spaces-count]");
+const numeros = document.querySelector("li[data-testid=number-count]");
+const suma = document.querySelector("li[data-testid=number-sum]");
+const longitudMedia = document.querySelector("li[data-testid=word-length-average]");
+const button = document.querySelector("reset-button");
 
-cajaTexto.addEventListener("input", function(){
-palabra.innerHTML = analyzer.getWordCount(cajaTexto.value);
-caracteres.innerHTML = analyzer.getCharacterCount(cajaTexto.value);
-caracteresSinEspacios.innerHTML = analyzer.getCharacterCountExcludingSpaces(cajaTexto.value);
-numeros.innerHTML = analyzer.getNumberCount(cajaTexto.value);
-suma.innerHTML = analyzer.getNumberSum(cajaTexto.value);
-longitudMedia.innerHTML = analyzer.getAverageWordLength(cajaTexto.value);
+cajaTexto.addEventListener("input", function () {
+  palabra.innerHTML = "Cantidad de palabras: " + analyzer.getWordCount(cajaTexto.value);
+  caracteres.innerHTML = "Cantidad de caracteres: " + analyzer.getCharacterCount(cajaTexto.value);
+  caracteresSinEspacios.innerHTML = "Cantidad de caracteres sin espacios: " + analyzer.getCharacterCountExcludingSpaces(cajaTexto.value);
+  numeros.innerHTML = "Números: " + analyzer.getNumberCount(cajaTexto.value);
+  suma.innerHTML = "Suma de números: " + analyzer.getNumberSum(cajaTexto.value);
+  longitudMedia.innerHTML = "Longitud de palabras: " + analyzer.getAverageWordLength(cajaTexto.value);
 
 });
 
-button.addEventListener('click', function(){
-cajaTexto.value = "";
-palabra.innerHTML = 0;
-caracteres.innerHTML = 0;
-caracteresSinEspacios.innerHTML = 0;
-numeros.innerHTML = 0;
+button.addEventListener("click", function () {
+  cajaTexto.value = " ";
+  palabra.innerHTML = 0;
+  caracteres.innerHTML = 0;
+  caracteresSinEspacios.innerHTML = 0;
+  numeros.innerHTML = 0;
 
 
 })
